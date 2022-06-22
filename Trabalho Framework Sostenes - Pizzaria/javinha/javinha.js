badbd0864fa3a5a0818a00249c4aa153059e8345
JulianaFDSB <92896809+JulianaFDSB@users.noreply.github.com>
2022-06-22 00:43:00 -0300

    Add files via upload

---
+++ Trabalho Framework Sostenes - Pizzaria/javinha/javinha.js
@@ -0,0 +1,34 @@
+
+ 
+
+
+function cadastrar(){
+		alert("Obrigado pelo cadastro!");
+	} 
+
+
+    function pizza(){
+        document.getElementById("pizzaria").src="img/pizza1.jpg";
+        setTimeout("pizza2()",2000);
+        }
+        function pizza2(){
+            document.getElementById("pizzaria").src="img/pizza2.jpg";
+            setTimeout("pizza3()",2000);
+            
+        }
+        function pizza3(){
+            document.getElementById("pizzaria").src="img/pizza3.jpg";
+            setTimeout("pizza4()",2000);
+            
+        }
+        function pizza4(){
+            document.getElementById("pizzaria").src="img/pizza4.jpg";
+            setTimeout("pizza5()",2000);
+            
+        }
+        function pizza5(){
+            document.getElementById("pizzaria").src="img/pizza5.jpg";
+            setTimeout("pizza()",2000);
+            
+		} 
+		 
